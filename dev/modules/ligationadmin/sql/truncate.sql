3ec783c049a5520278546e76fb50a1165edee655
caleb <calebdevelop@gmail.com>
2017-01-06 20:36:55 +0100

    rm

--- dev/modules/ligationadmin/sql/truncate.sql
+++
@@ -1,6 +0,0 @@
-TRUNCATE TABLE `ps_la_ligation_lang`;
-TRUNCATE TABLE `ps_la_history`;
-TRUNCATE TABLE `ps_la_history_product`;
-TRUNCATE TABLE `ps_la_ligation`;
-TRUNCATE TABLE `ps_la_ligation_lang`;
-TRUNCATE TABLE `ps_la_products`;
--- dev/modules/shippingengine/views/templates/front/order-step1.tpl
+++
@@ -1,6 +0,0 @@
-{*
- * Created by PhpStorm.
- * User: Anthony
- * Date: 14/11/2016
- * Time: 18:26
- *}
